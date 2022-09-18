3e5586e765489e90f58ef049c4af0fe4bf480f45
Guidoperezorue <78152166+Guidoperezorue@users.noreply.github.com>
2022-09-17 22:26:03 -0300

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,14 @@
+// Crear Set con nombres de familia
+const mySet = new Set (["Nancy", "Ricardo", "Denise", "Guido"])
+
+console.log(mySet)
+
+//Modificar el set añadiendo mi nombre (duplicado) (debería darte lo mismo)
+mySet.add("Guido")
+
+console.log(mySet)
+
+//Modificar el set añadiendo el nombre "Javascript"
+mySet.add("Javascript")
+
+console.log(mySet)
